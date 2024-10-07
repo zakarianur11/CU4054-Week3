3270fd5c1b8ff7035f646b787f8ebe26d8f2a884
zakarianur11 <152890184+zakarianur11@users.noreply.github.com>
2024-10-07 16:07:03 +0100

    3Tasks Done

--- RobbyTheRobot/Source.cpp
+++ RobbyTheRobot/Source.cpp
@@ -10,13 +10,27 @@ GraphicsManager* picture;
 int main()
 {
 	//declare the room, robby and picture objects here
-	room = new Room(0);
+	room = new Room(2);
 	robby = new Robot();
 	picture = new GraphicsManager(room, robby);
 
 
-	robby->move();
+
+	for (int i = 0; i < 2; i++)
+	{
+		robby->move();
+		picture->draw();
+	}
+
+	robby->right();
 	picture->draw();
+
+
+	for (int i = 0; i < 2; i++)
+	{
+		robby->move();
+		picture->draw();
+	}
 
 
 	//we need system pause so that we can see where robby ends up
---
+++ RobbyTheRobot/Source1.cpp
@@ -0,0 +1,12 @@
+//#include<iostream>
+//
+//int main()
+//{
+//	for (int i = 99; i >= 0; i--)
+//	{
+//		std::cout << i<<"bottles of beer on the wall"<<i<<"bottles of beer" << std::endl;
+//		std::cout << "Take one down pass it around, " << i - 1 << "bottles of beer on the wall" << std::endl;
+//	}
+//
+//	system("Pause");
+//}
